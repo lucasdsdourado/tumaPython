9674bc70d2c8a196e46fbdb6295097cf8c83faf0
Lucas Dourado <70813207+lucasdsdourado@users.noreply.github.com>
2025-10-23 23:18:35 -0300

    Add files via upload

---
+++ anotacoes/recursividade.py
@@ -0,0 +1,9 @@
+def contagem_regressiva(n):
+    if n <= 0:
+        print("Fim")
+    else:
+        print(n)
+        contagem_regressiva(n-1)
+
+contagem_regressiva(5)
+
---
+++ anotacoes/teste.py
@@ -0,0 +1,4 @@
+numero = 20
+print("Olá Mundo ", 5  +  10)
+print("Olá Mundo ", 20 / 2)
+print("Olá Mundo ", 50 - 30)
